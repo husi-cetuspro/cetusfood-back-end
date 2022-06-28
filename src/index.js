38a610d1b88c00deed7e8cff267a41f55d888204
rxtthin <rotthin_dev@protonmail.com>
2022-06-28 11:28:10 +0200

    Initial SQLite3 integration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
+const db = require('./db.js');
 
-app.listen(8088);
+app.post('/admin/addRestaurant', (req, res) => {
+	res.send('test');
+	res.end();
+});
+
+app.listen(8688, () => {
+	db.createDb();
+});
