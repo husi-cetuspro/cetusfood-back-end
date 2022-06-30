1343e2dabc89768ff6d3b1cea28ba17f98191b03
rxtthin <rotthin_dev@protonmail.com>
2022-06-30 09:20:01 +0200

    Changed PUT /orders to POST /orders for adding a new order

--- src/api/orders/orders.controller.ts
+++ src/api/orders/orders.controller.ts
@@ -9,9 +9,9 @@ import { ApiOkResponse, ApiOperation, ApiTags, } from '@nestjs/swagger';
 export class OrdersController {
 	constructor(private readonly ordersService: OrdersService) {}
 
-	@Put()
+	@Post()
 	@ApiOperation({summary: "Dodaje zamowienie do bazy danych"})
-	@ApiOkResponse({description: "Zamowienie, ktore został dodany do bazy danych", type: Order, isArray: false})
+	@ApiOkResponse({description: "Zamowienie, ktore zostało dodane do bazy danych", type: Order, isArray: false})
 	public async add(@Body() Order: Order): Promise<Order> {
 		return await this.ordersService.add(Order);
 	}
